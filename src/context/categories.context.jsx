acc97ec7c42b247cadb021676f31981cd69ea865
Raitis Iziks <raitisiziks@gmail.com>
2024-04-06 15:07:03 +0300

    change useState to useReducer

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -1,22 +1,42 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
-import {
-  addCollectionAndDocuments,
-  getCategoriesAndDocuments,
-} from "../utils/firebase/firebase.utils.jsx";
-
-// import SHOP_DATA from "../shop-data.js";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
-export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+const CATEGORIES_ACTION_TYPES = {
+  SET_CATEGORIES_MAP: "SET_CATEGORIES_MAP",
+};
 
-  // useEffect(() => {
-  //   addCollectionAndDocuments("categories", SHOP_DATA);
-  // }, []);
+const INITIAL_STATE = {
+  categoriesMap: {},
+};
+
+const categoriesReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP:
+      return {
+        ...state,
+        categoriesMap: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in categoriesReducer`);
+  }
+};
+
+export const CategoriesProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(categoriesReducer, INITIAL_STATE);
+
+  const setCategoriesMap = (categoriesMap) => {
+    dispatch(
+      createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categoriesMap)
+    );
+  };
 
   useEffect(() => {
     const getCategoriesMap = async () => {
@@ -27,7 +47,7 @@ export const CategoriesProvider = ({ children }) => {
     getCategoriesMap();
   }, []);
 
-  const value = { categoriesMap };
+  const value = { categoriesMap: state.categoriesMap };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
