a3f49dc719a929d0b8bbb7c2b81232d700803928
Raitis Iziks <raitisiziks@gmail.com>
2024-04-07 21:08:48 +0300

    add hook useOutsideClick to close dropdown when I click outside of it

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,11 @@
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import useOutsideClick from "./useOutsideClick";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { selectCartItems } from "../../store/cart/cart.selector";
+import { setIsCartOpen } from "../../store/cart/cart.action";
 
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -14,14 +18,18 @@ import {
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const cartRef = useRef();
+
+  useOutsideClick(cartRef, () => dispatch(setIsCartOpen(false)));
 
   const goToCheckoutHandler = () => {
     navigate("/checkout");
   };
 
   return (
-    <CartDropdownContainer>
+    <CartDropdownContainer ref={cartRef}>
       <CartItems>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
